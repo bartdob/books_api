6e42fe24d26f9dce64442f1a71796a6fb8e04183
Bart <44099512+bartdob@users.noreply.github.com>
2022-04-28 14:16:05 +0200

    Update test_user.py

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 
-def test_should_create_user_with_username(db) -> None:
-    user = User.objects.create_user("Haki")
-    assert user.username == "Haki"
+# def test_should_create_user_with_username(db) -> None:
+#     user = User.objects.create_user("Haki")
+#     assert user.username == "Haki"
